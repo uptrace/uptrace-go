10d6a474e30b78f44b37fe4b74e8a82a1c14f3c5
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-09-04 17:35:00 +0300

    Add Gin example

---
+++ example/gin/README.md
@@ -0,0 +1,13 @@
+# Gin instrumentation example
+
+To run this example:
+
+```bash
+UPTRACE_DSN="https://<key>@uptrace.dev/<project_id>" make
+```
+
+You can access the server at http://localhost:9999:
+
+```bash
+curl -v http://localhost:9999/profiles/admin
+```
---
+++ example/gin/server.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/uptrace/uptrace-go/uptrace"
+	otelgin "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/label"
+)
+
+const profileTmpl = "profile"
+
+var (
+	upclient *uptrace.Client
+	tracer   = global.Tracer("gin-tracer")
+)
+
+func main() {
+	ctx := context.Background()
+
+	log.Printf("using UPTRACE_DSN=%q", os.Getenv("UPTRACE_DSN"))
+
+	hostname, _ := os.Hostname()
+	upclient = uptrace.NewClient(&uptrace.Config{
+		// copy your project DSN here or use UPTRACE_DSN env var
+		DSN: "",
+
+		Resource: map[string]interface{}{
+			"hostname": hostname,
+		},
+	})
+	defer upclient.Close()
+
+	// Make sure!
+	tracer = upclient.Tracer("gin-tracer")
+
+	router := gin.Default()
+	router.SetHTMLTemplate(profileTemplate())
+	router.Use(otelgin.Middleware("service-name"))
+	router.GET("/profiles/:username", userProfileEndpoint)
+
+	if err := router.Run(":9999"); err != nil {
+		upclient.ReportError(ctx, err)
+	}
+}
+
+func profileTemplate() *template.Template {
+	tmpl := `<html><h1>Hello {{ .username }} {{ .name }}</h1></html>` + "\n"
+	return template.Must(template.New(profileTmpl).Parse(tmpl))
+}
+
+func userProfileEndpoint(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	username := c.Param("username")
+	name, err := selectUser(ctx, username)
+	if err != nil {
+		c.AbortWithError(http.StatusNotFound, err)
+		return
+	}
+
+	otelgin.HTML(c, http.StatusOK, profileTmpl, gin.H{
+		"username": username,
+		"name":     name,
+	})
+}
+
+func selectUser(ctx context.Context, username string) (string, error) {
+	_, span := tracer.Start(ctx, "selectUser")
+	defer span.End()
+
+	span.SetAttributes(label.String("username", username))
+
+	if username == "admin" {
+		return "Joe", nil
+	}
+
+	return "", fmt.Errorf("username=%s not found", username)
+}
