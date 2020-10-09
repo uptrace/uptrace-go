ea884557aa8484551d6e6d9c266f6ddb26e9ddae
Anatolii Mihailenco <anmic.webdev@gmail.com>
2020-10-09 17:19:55 +0300

    Add go-redis cluster example

---
+++ example/go-redis-cluster/README.md
@@ -0,0 +1,7 @@
+# Go-redis cluster instrumentation example
+
+To run this example:
+
+```bash
+UPTRACE_DSN="https://<key>@uptrace.dev/<project_id>" make
+```
---
+++ example/go-redis-cluster/client.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/go-redis/redisext"
+	"github.com/uptrace/uptrace-go/uptrace"
+	"go.opentelemetry.io/otel/api/global"
+)
+
+var tracer = global.Tracer("go-redis-cluster-tracer")
+
+func main() {
+	ctx := context.Background()
+
+	upclient := setupUptrace()
+	defer upclient.Close()
+	defer upclient.ReportPanic(ctx)
+
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{"redis-node-0:6379", "redis-node-1:6379", "redis-node-2:6379"},
+
+		NewClient: func(opt *redis.Options) *redis.Client {
+			node := redis.NewClient(opt)
+			node.AddHook(redisext.OpenTelemetryHook{})
+			return node
+		},
+	})
+	defer rdb.Close()
+
+	rdb.AddHook(redisext.OpenTelemetryHook{})
+
+	ctx, span := tracer.Start(ctx, "redis-main-span")
+	defer span.End()
+
+	if err := redisCommands(ctx, rdb); err != nil {
+		upclient.ReportError(ctx, err)
+		log.Println(err.Error())
+		return
+	}
+
+	fmt.Println("trace", upclient.TraceURL(span))
+}
+
+func setupUptrace() *uptrace.Client {
+	if os.Getenv("UPTRACE_DSN") == "" {
+		panic("UPTRACE_DSN is empty or missing")
+	}
+
+	hostname, _ := os.Hostname()
+	upclient := uptrace.NewClient(&uptrace.Config{
+		// copy your project DSN here or use UPTRACE_DSN enar
+		DSN: "",
+
+		Resource: map[string]interface{}{
+			"host.name": hostname,
+		},
+	})
+
+	return upclient
+}
+
+func redisCommands(ctx context.Context, rdb *redis.ClusterClient) error {
+	if err := rdb.Set(ctx, "foo", "bar", 0).Err(); err != nil {
+		return err
+	}
+
+	if err := rdb.Get(ctx, "foo").Err(); err != nil {
+		return err
+	}
+
+	if _, err := rdb.Pipelined(ctx, func(pipe redis.Pipeliner) error {
+		pipe.Set(ctx, "foo", "bar2", 0)
+		pipe.Get(ctx, "foo")
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
