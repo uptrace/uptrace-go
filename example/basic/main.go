44587ab2e9a8f8597f69c5563283d0992a2280d8
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2021-02-03 15:16:24 +0200

    Cleanup basic example

--- example/basic/main.go
+++ example/basic/main.go
@@ -8,51 +8,40 @@ import (
 	"github.com/uptrace/uptrace-go/uptrace"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/label"
-	"go.opentelemetry.io/otel/trace"
 )
 
 func main() {
 	ctx := context.Background()
-	upclient := newUptraceClient()
 
-	defer upclient.Close()
-	defer upclient.ReportPanic(ctx)
-
-	// Use upclient to report errors when there is no active span.
-	upclient.ReportError(ctx, errors.New("Hello from uptrace-go!"))
-
-	// Create a tracer.
-	tracer := otel.Tracer("github.com/your/repo")
-
-	// Start active span.
-	ctx, span := tracer.Start(ctx, "main span")
-
-	{
-		_, span := tracer.Start(ctx, "child1")
-		span.SetAttributes(label.String("key1", "value1"))
-		span.AddEvent("event-name", trace.WithAttributes(label.String("foo", "bar")))
-		span.End()
-	}
-
-	{
-		_, span := tracer.Start(ctx, "child2")
-		span.SetAttributes(label.String("key2", "value2"))
-		span.AddEvent("event-name", trace.WithAttributes(label.String("foo", "baz")))
-		span.End()
-	}
-
-	span.End()
-	fmt.Printf("trace: %s\n", upclient.TraceURL(span))
-}
-
-func newUptraceClient() *uptrace.Client {
 	upclient := uptrace.NewClient(&uptrace.Config{
 		// copy your project DSN here or use UPTRACE_DSN env var
 		DSN: "",
 
-		ServiceName:    "test",
+		ServiceName:    "myservice",
 		ServiceVersion: "v1.0.0",
 	})
 
-	return upclient
+	defer upclient.Close()
+	defer upclient.ReportPanic(ctx)
+
+	// Use upclient to report errors when there are no spans.
+	upclient.ReportError(ctx, errors.New("Hello from uptrace-go"))
+
+	// Create a tracer.
+	tracer := otel.Tracer("github.com/your/repo")
+
+	// Start a span.
+	ctx, span := tracer.Start(ctx, "main span")
+
+	_, child1 := tracer.Start(ctx, "child1")
+	child1.SetAttributes(label.String("key1", "value1"))
+	child1.RecordError(errors.New("error1"))
+	child1.End()
+
+	_, child2 := tracer.Start(ctx, "child2")
+	child2.SetAttributes(label.Int("key2", 42), label.Float64("key3", 123.456))
+	child2.End()
+
+	span.End()
+	fmt.Printf("trace: %s\n", upclient.TraceURL(span))
 }
