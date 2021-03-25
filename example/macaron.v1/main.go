a86cd32e1ed92f0d7c7d15f49faff9953b7f14f0
Anatolii Mihailenco <anmic.webdev@gmail.com>
2021-03-25 13:52:05 +0200

    Add trace URL.

--- example/macaron.v1/main.go
+++ example/macaron.v1/main.go
@@ -6,7 +6,6 @@ import (
 
 	"go.opentelemetry.io/contrib/instrumentation/gopkg.in/macaron.v1/otelmacaron"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"gopkg.in/macaron.v1"
 
@@ -25,33 +24,40 @@ func main() {
 	defer uptrace.Shutdown(ctx)
 
 	m := macaron.Classic()
-	m.Get("/profiles/:username", userProfileEndpoint)
+	m.Get("/", indexHandler)
+	m.Get("/hello/:username", helloHandler)
 	m.Use(otelmacaron.Middleware("service-name"))
 
 	m.Run(9999)
 }
 
-func userProfileEndpoint(c *macaron.Context) string {
+func indexHandler(c *macaron.Context) string {
 	ctx := c.Req.Context()
 
-	username := c.Params("username")
-	name, err := selectUser(ctx, username)
-	if err != nil {
-		trace.SpanFromContext(ctx).RecordError(err)
-	}
-
-	return fmt.Sprintf(`<html><h1>Hello %s %s </h1></html>`+"\n", username, name)
+	traceURL := uptrace.TraceURL(trace.SpanFromContext(ctx))
+	tmpl := `
+	<html>
+	<p>Here are some routes for you:</p>
+	<ul>
+		<li><a href="/hello/world">Hello world</a></li>
+		<li><a href="/hello/foo-bar">Hello foo-bar</a></li>
+	</ul>
+	<p><a href="%s" target="_blank">%s</a></p>
+	</html>
+	`
+	return fmt.Sprintf(tmpl, traceURL, traceURL)
 }
 
-func selectUser(ctx context.Context, username string) (string, error) {
-	_, span := tracer.Start(ctx, "selectUser")
-	defer span.End()
+func helloHandler(c *macaron.Context) string {
+	ctx := c.Req.Context()
 
-	span.SetAttributes(attribute.String("username", username))
-
-	if username == "admin" {
-		return "Joe", nil
-	}
-
-	return "", fmt.Errorf("username=%s not found", username)
+	traceURL := uptrace.TraceURL(trace.SpanFromContext(ctx))
+	username := c.Params("username")
+	tmpl := `
+	<html>
+	<h3>Hello %s</h3>
+	<p><a href="%s" target="_blank">%s</a></p>
+	</html>
+	`
+	return fmt.Sprintf(tmpl, username, traceURL, traceURL)
 }
