ef84fd46a4fd0eb14943c87377e1f3c325749f81
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-12-09 13:38:47 +0200

    chore: enable update

--- scripts/release.sh
+++ scripts/release.sh
@@ -46,11 +46,11 @@ PACKAGE_DIRS=$(find . -mindepth 1 -type f -name 'go.mod' -exec dirname {} \; \
   | sed 's/^\.\///' \
   | sort)
 
-# for dir in $PACKAGE_DIRS
-# do
-#     printf "${dir}: go get -u && go mod tidy\n"
-#     (cd ./${dir} && go get -u ./... && go mod tidy -compat=1.18)
-# done
+for dir in $PACKAGE_DIRS
+do
+    printf "${dir}: go get -u && go mod tidy\n"
+    (cd ./${dir} && go get -u ./... && go mod tidy -compat=1.18)
+done
 
 for dir in $PACKAGE_DIRS
 do
