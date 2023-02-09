b99fc03f47b7cae64cd9f5a55746a1615c2ea8e1
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2023-02-09 08:40:10 +0200

    chore: tweak release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -48,8 +48,8 @@ PACKAGE_DIRS=$(find . -mindepth 1 -type f -name 'go.mod' -exec dirname {} \; \
 
 for dir in $PACKAGE_DIRS
 do
-    printf "${dir}: go get -u && go mod tidy\n"
-    (cd ./${dir} && go get -u ./... && go mod tidy -compat=1.18)
+    printf "${dir}: go mod tidy\n"
+    (cd ./${dir} && go mod tidy -compat=1.18)
 done
 
 for dir in $PACKAGE_DIRS
