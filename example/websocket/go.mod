ffa5c10c25cccdf0c0016b4bfd2e234dc019cf58
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2024-08-26 14:28:47 +0300

    chore: add websocket example

---
+++ example/websocket/go.mod
@@ -0,0 +1,41 @@
+module github.com/uptrace/uptrace-go/example/basic
+
+go 1.22
+
+toolchain go1.22.4
+
+replace github.com/uptrace/uptrace-go => ../..
+
+require (
+	github.com/uptrace/uptrace-go v1.28.0
+	go.opentelemetry.io/contrib/bridges/otelslog v0.4.0
+	go.opentelemetry.io/otel v1.29.0
+	go.opentelemetry.io/otel/trace v1.29.0
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.54.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.5.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.29.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.29.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.29.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.29.0 // indirect
+	go.opentelemetry.io/otel/log v0.5.0 // indirect
+	go.opentelemetry.io/otel/metric v1.29.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.29.0 // indirect
+	go.opentelemetry.io/otel/sdk/log v0.5.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.29.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.3.1 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240823204242-4ba0660f739c // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240823204242-4ba0660f739c // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
---
+++ example/websocket/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/uptrace/uptrace-go/uptrace"
+)
+
+var tracer = otel.Tracer("app_or_package_name")
+
+func main() {
+	ctx := context.Background()
+
+	// Configure OpenTelemetry with sensible defaults.
+	uptrace.ConfigureOpentelemetry(
+		// copy your project DSN here or use UPTRACE_DSN env var
+		// uptrace.WithDSN("https://<token>@uptrace.dev/<project_id>"),
+
+		uptrace.WithServiceName("myservice"),
+		uptrace.WithServiceVersion("1.0.0"),
+	)
+	// Send buffered spans and free resources.
+	defer uptrace.Shutdown(ctx)
+
+	// Start a root span for a websocket connection.
+	ctx, span := tracer.Start(ctx, "websocket", trace.WithSpanKind(trace.SpanKindServer))
+	defer span.End()
+
+	fmt.Printf("websocket connection: %s\n", uptrace.TraceURL(span))
+
+	handleWebsocketRequest(ctx)
+	handleWebsocketRequest(ctx)
+	handleWebsocketRequest(ctx)
+}
+
+func handleWebsocketRequest(ctx context.Context) {
+	// Create another span so we can end it separately from the root span.
+	// The root span can stay open for hours.
+	ctx, span := tracer.Start(ctx, "websocket-request", trace.WithSpanKind(trace.SpanKindServer))
+	defer span.End()
+
+	// Save parent context.
+	parentSpan := span
+
+	// Create a separate trace for each websocket request.
+	ctx, span = tracer.Start(ctx, "websocket-request",
+		trace.WithSpanKind(trace.SpanKindServer),
+		trace.WithNewRoot())
+	defer span.End()
+
+	fmt.Printf("websocket request: %s\n", uptrace.TraceURL(span))
+
+	parentSpan.AddLink(trace.LinkFromContext(ctx))
+	span.SetAttributes(
+		attribute.String("parent_trace_id", parentSpan.SpanContext().TraceID().String()),
+	)
+
+	time.Sleep(time.Second)
+}
